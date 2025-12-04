7c9ff0a93a842ebefcf81430d2462ea503f62a56
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2025-12-04 22:08:24 +0200

    add Loader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,38 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchMovies } from '../../services/movieService';
 import type { Movie } from '../../types/movie';
 import SearchBar from '../SearchBar/SearchBar';
 import './App.module.css';
 import toast, { Toaster } from 'react-hot-toast';
+import Loader from '../Loader/Loader';
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoader, setIsLoader] = useState(false);
+
   const handleSearch = async (query: string) => {
     try {
+      setIsLoader(true);
       const responce = await fetchMovies(query);
 
-      if(responce.length === 0){
-      toast('No movies found for your request.', {
-        duration: 3000,
-        position: 'top-center',
-        style: {
-          background: '#fff',
-          marginTop: '100px',
-          fontSize: '18px',
-          padding: '16px 20px',
-          color: 'black',
-          fontWeight: '700',
-          border: '2px solid black',
-          borderRadius: '8px',
-        },
-      });
-      return;
-    }
-
-
+      if (responce.length === 0) {
+        toast('No movies found for your request.', {
+          duration: 3000,
+          position: 'top-center',
+          style: {
+            background: '#fff',
+            marginTop: '100px',
+            fontSize: '18px',
+            padding: '16px 20px',
+            color: 'black',
+            fontWeight: '700',
+            border: '2px solid black',
+            borderRadius: '8px',
+          },
+        });
+        return;
+      }
 
       setMovies(responce);
-      console.log(movies)
+
       return responce;
     } catch (err: unknown) {
       if (err instanceof Error) {
@@ -40,12 +42,17 @@ const App = () => {
       } else {
         console.log(String(err));
       }
+    } finally {
+      setIsLoader(false);
     }
   };
-
+  useEffect(() => {
+    console.log('Movies updated:', movies);
+  }, [movies]);
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
+      {isLoader && <Loader />}
       <Toaster />
     </>
   );
