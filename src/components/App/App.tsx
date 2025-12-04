8706905670c5153de24fc65319c0f42b31ad5ee2
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2025-12-04 21:50:21 +0200

    add notification(Toaster) empty Search query

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,34 @@ import { fetchMovies } from '../../services/movieService';
 import type { Movie } from '../../types/movie';
 import SearchBar from '../SearchBar/SearchBar';
 import './App.module.css';
+import toast, { Toaster } from 'react-hot-toast';
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const handleSearch = async (query: string) => {
     try {
       const responce = await fetchMovies(query);
+
+      if(responce.length === 0){
+      toast('No movies found for your request.', {
+        duration: 3000,
+        position: 'top-center',
+        style: {
+          background: '#fff',
+          marginTop: '100px',
+          fontSize: '18px',
+          padding: '16px 20px',
+          color: 'black',
+          fontWeight: '700',
+          border: '2px solid black',
+          borderRadius: '8px',
+        },
+      });
+      return;
+    }
+
+
+
       setMovies(responce);
       console.log(movies)
       return responce;
@@ -24,6 +46,7 @@ const App = () => {
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
+      <Toaster />
     </>
   );
 };
