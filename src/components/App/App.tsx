0b9296c7371852b9909abb2454eb78ca3dad9582
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2025-12-04 21:44:31 +0200

    add fetchMovies, interface Movie, useState movies, env

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
 import SearchBar from '../SearchBar/SearchBar';
 import './App.module.css';
 
 const App = () => {
-  const handleSearch = (query: string) => {
-    console.log(query);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const handleSearch = async (query: string) => {
+    try {
+      const responce = await fetchMovies(query);
+      setMovies(responce);
+      console.log(movies)
+      return responce;
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.log(err.message);
+      } else {
+        console.log(String(err));
+      }
+    }
   };
 
   return (
